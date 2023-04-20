4e7dc7c7761e37595fcb40a4e82343c304804d13
mercelo13 <marcelo.garciar@sansano.usm.cl>
2023-04-20 14:25:23 -0400

    Resolucion certamen-1

--- src/repository/usersRepository.js
+++ src/repository/usersRepository.js
@@ -1,5 +1,27 @@
 import dataset from '../dataset/users_info.json'
 
-export default function getUsersFromDataset () {
-    return dataset
+export default function getUsersFromDataset() {
+    orderedad = orderedad.toLowerCase();
+    if (orderedad === 'asc'){
+        let users = []
+        dataset.forEach(element => {
+            if((element["name"].toLowerCase()).includes(input)){
+                users.push({
+                    "isActive": element.isActive,
+                    "age": element.age,
+                    "name": element.name,
+                    "gender": element.gender,
+                    "company": element.company,
+                });
+            }
+        });
+        if(orderedad==='asc'){  //Usando la función sort se ordenan por fecha de creación ascendente o descendentemente según corresponda
+            users.sort((a, b) => (a.age > b.age) ? 1 : -1)
+            response.status(200).json(users) 
+        }
+        else{
+            console.log("ERROR")
+        }
+    }
+    return users
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,9 @@
 import Router from 'koa-router'
 import getHealth from './health/health'
-
+import getUsersFromDataset from './../repository/usersRepository'
 const router = new Router()
 
 router.get('/health', getHealth)
 
+router.get('api/users/:name', getUsersFromDataset)
 export default router
